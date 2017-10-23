42c96d2a75a0bc1469635065a435c28d3073542a
Psakkos <31419207+Psakkos@users.noreply.github.com>
2017-10-23 14:42:07 -0500

    Add files via upload

--- DistributionCenter.js
+++ DistributionCenter.js
@@ -15,31 +15,44 @@ function configureWareHouse(numberofitems){
 function configureTruckFleet(numberoftrucks){
     let theFleet = new Queue();
     //fill the fleet with different sized trucks using math.random()
-    for(let x=0;x<numberofitems;x++){
+    for(let x=0;x<numberoftrucks;x++){
       theFleet.enqueue(new Truck());
     }
     return theFleet;
 }
 function distribute(wh,tf){
-  tempTruck;
-  for(let c=0; c++){
-    
+  let ready = new Queue;
+  while(!wh.isEmpty() && !tf.isEmpty()){
+    let currentTruck = tf.dequeue();
+    while(currentTruck.spaceEfficiency()<1){
+      currentTruck.inventory.push(Warehouse.dequeue);
+      if(currentTruck.spaceEfficiency()>1);{
+        Warehouse.enque(currentTruck.inventory.pop());
+      }
+    }
+    ready.enqueue(currentTruck);
+  }
+  for(let x=0; inventory.length;x++){
+    spaceTakenUp= Product.space;
   }
     //distribute the products in the warehouse to the truck fleet.
-}
+
 function ship(fleet,percent){
     //if the trucks spaceEfficency is greater than or equal to the percent, then remove the truck from the fleet's linked list
 }
 function main(){
     let flemhouse = configureWareHouse();
     let flemfleet = configureTruckFleet();
-    flemfleet = distribute(flemhouse,flemfleet);
-    let shipping = ship(flemfleet,0.80);
-    for(let s=0;s<shipping.length;s++){
+    let ready = distribute(flemhouse,flemfleet);
+    for(let s=0;s<ready.length;s++){
         console.log("Truck "+s+" is in route");
         console.log("-----------------------");
-        while(s.getNext() != null){
-            console.log(shipping[s].getValue().spaceEfficiency());
+        while(!ready.isEmpty()){
+          let ct=ready.dequeue();
+            console.log(ct.spaceEfficiency());
+            for(let x=0; x<ct.inventory.length; x++){
+              console.log(ct.inventory[x]);
+
         }
     }
 }
